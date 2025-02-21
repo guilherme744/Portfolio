eea83ede915541299b2261ce95b6e50958774d59
guilherme <guilherme2012478@gmail.com>
2025-02-21 11:37:51 +0900

    ....

--- src/utils/api/splitbeeApi.ts
+++ src/utils/api/splitbeeApi.ts
@@ -2,7 +2,7 @@ const NOTION_BLOG_ID = process.env.NEXT_PUBLIC_NOTION_BLOG_ID
 
 export const getAllPosts = async () => {
     const res = await fetch(`https://worker-notion.fabioraminhuk.workers.dev/v1/table/${NOTION_BLOG_ID}`,
-        { next: { revalidate: 3000 }}
+        { next: { revalidate: 6000 }}
     )
 
     if (!res.ok) {
@@ -20,7 +20,7 @@ export const getAllPosts = async () => {
 
 export const getPosts = async (id: string) => {
     const res = await fetch(`https://worker-notion.fabioraminhuk.workers.dev/v1/page/${id}`,
-        { next: { revalidate: 3000 } }
+        { next: { revalidate: 6000 } }
     )
 
     if (!res.ok) {
@@ -31,7 +31,7 @@ export const getPosts = async (id: string) => {
     if (!contentType || !contentType.includes('application/json')) {
         throw new Error(`Invalid response format: Expected JSON but received ${contentType}`)
     }
-    
+
     const post = await res.json()
     return post
 }
