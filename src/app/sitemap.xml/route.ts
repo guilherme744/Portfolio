51b097c443e649591e11625ed46f22e3cfcb2686
guilherme <guilherme2012478@gmail.com>
2025-02-21 11:07:16 +0900

    prettier

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,11 +1,10 @@
+import { BlogPost } from "@/@types/blog";
+import { getAllPosts } from "@/utils/api/splitbeeApi";
 
-import { BlogPost } from '@/@types/blog'
-import { getAllPosts } from '@/utils/api/splitbeeApi'
-
-const URL = 'https://fabra.dev'
+const URL = "https://fabra.dev";
 
 function generateSiteMap(posts: BlogPost[]) {
-    return `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>${URL}</loc>
@@ -73,33 +72,39 @@ function generateSiteMap(posts: BlogPost[]) {
       <changefreq>yearly</changefreq>
       <priority>0.5</priority>
     </url>
-     ${posts && posts.length > 0 ? posts.map(({ slug,date }) => {
-        return `
+     ${
+       posts && posts.length > 0
+         ? posts
+             .map(({ slug, date }) => {
+               return `
            <url>
                 <loc>${`${URL}/blog/${slug}`}</loc>
                 <lastmod>${date}</lastmod>
                 <changefreq>weekly</changefreq>
                 <priority>0.5</priority>
            </url>
-         `
-    }).join(''): ''}
+         `;
+             })
+             .join("")
+         : ""
+     }
    </urlset>
- `
+ `;
 }
 
 export async function GET() {
   try {
-      const posts = await getAllPosts()
-      const body = generateSiteMap(posts)
-      return new Response(body, {
-          status: 200,
-          headers: {
-              'Cache-control': 'public, s-maxage=86400, stale-while-revalidate',
-              'content-type': 'application/xml',
-          },
-      })
+    const posts = await getAllPosts();
+    const body = generateSiteMap(posts);
+    return new Response(body, {
+      status: 200,
+      headers: {
+        "Cache-control": "public, s-maxage=86400, stale-while-revalidate",
+        "content-type": "application/xml",
+      },
+    });
   } catch (error) {
-      console.error("Error fetching posts:", error)
-      return new Response("Failed to generate sitemap", { status: 500 })
+    console.error("Error fetching posts:", error);
+    return new Response("Failed to generate sitemap", { status: 500 });
   }
 }
